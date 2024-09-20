4c8f988f62955a3ed52ce962e83698c061842823
richie <richie130283@gmail.com>
2024-09-20 07:09:56 +0700

    add

--- js/extra.js
+++ js/extra.js
@@ -7,13 +7,56 @@ function changeLanguage(lang) {
 document.getElementById('langButton').addEventListener('click', function() {
     if (this.innerText === 'EN') {
         this.innerText = 'ID';
-        this.classList.remove('btn-success');
-        this.classList.add('btn-light');
+        this.classList.remove('btn-light');
+        this.classList.add('btn-success');
+        // this.classList.remove('btn-success');
+        // this.classList.add('btn-light');
         changeLanguage('en');
     } else {
         this.innerText = 'EN';
         this.classList.remove('btn-light');
         this.classList.add('btn-success');
+        // this.classList.remove('btn-success');
+        // this.classList.add('btn-light');
         changeLanguage('id');
     }
 });
+
+// document.getElementById('langButton').addEventListener('touchstart', function() {
+//     if (this.innerText === 'EN') {
+//         this.innerText = 'ID';
+//         this.classList.remove('btn-success');
+//         this.classList.add('btn-light');
+//         changeLanguage('en');
+//     } else {
+//         this.innerText = 'EN';
+//         this.classList.remove('btn-light');
+//         this.classList.add('btn-success');
+//         changeLanguage('id');
+//     }
+// });
+
+
+// const langToggle = document.getElementById('langButton');
+// const englishContent = document.querySelectorAll('[data-lang="en"]');
+// const spanishContent = document.querySelectorAll('[data-lang="id"]');
+
+// langToggle.addEventListener('click', () => {
+//   if (langToggle.textContent === 'EN') {
+//     englishContent.forEach(element => {
+//       element.style.display = 'block';
+//     });
+//     spanishContent.forEach(element => {
+//       element.style.display = 'none';
+//     });
+//     langToggle.textContent = 'ID';
+//   } else {
+//     englishContent.forEach(element => {
+//       element.style.display = 'none';
+//     });
+//     spanishContent.forEach(element => {
+//       element.style.display = 'block';
+//     });
+//     langToggle.textContent = 'EN';
+//   }
+// });
